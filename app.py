84a66ff67b35f6097027bde5529b5b0b05eb7343
Topo Frank <florianfranklin@gmail.com>
2025-10-12 14:57:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -392,7 +392,7 @@ def create_polygon_plot(results_df, ref_x, ref_y, lang='en'):
     arrowsize=1,
     arrowwidth=1.5,
     arrowcolor='rgba(0,100,200,0.5)',
-    text=f"Az: {row['Azimuth_Decimal']:.1f}°",
+    text= "",
     font=dict(size=15, color='darkblue'),
     bgcolor='rgba(0,0,0,0)',  # ✅ Fondo completamente transparente
     borderpad=0,              # ✅ Sin relleno
