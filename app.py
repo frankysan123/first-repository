eb4fff5c6100cdce96f39a9b8f654fe325236817
Topo Frank <florianfranklin@gmail.com>
2025-10-21 08:42:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,7 @@ def create_multi_point_plot(single_points, results_df, ref_x, ref_y, x_coord, y_
         ),
         hovermode='closest',
         height=800,  # Aumentado de 700 a 1000 para un gráfico más grande
-        width=1400, # Aumentado de 700 a 1000 para un gráfico más grande
+        width=1500, # Aumentado de 700 a 1000 para un gráfico más grande
         yaxis=dict(scaleanchor="x", scaleratio=1),
         plot_bgcolor='rgba(240,240,240,0.5)',
         dragmode='pan'
